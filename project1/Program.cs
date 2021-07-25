727ee18e0867aee407c76ea0e4ed47e7d4409975
chait <chait@LAPTOP-LFUODKCE>
2021-07-25 15:08:21 +0530

    Add project files.

---
+++ project1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("here is a simple program");
+            Console.ReadLine();
+        }
+    }
+}
